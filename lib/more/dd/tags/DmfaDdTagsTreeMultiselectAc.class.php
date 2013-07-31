a49b698bcaf71a25b85d3267b2dd5b22a9f821ab
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2013-07-31 10:42:31 +0400

    ...

--- lib/more/dd/tags/DmfaDdTagsTreeMultiselectAc.class.php
+++ lib/more/dd/tags/DmfaDdTagsTreeMultiselectAc.class.php
@@ -2,18 +2,8 @@
 
 class DmfaDdTagsTreeMultiselectAc extends DmfaDdTagsTreeMultiselect {
 
-  protected function deleteTagItems_($k) {
-    die2($k);
-  }
-
-  /*
   function afterCreateUpdate($value, $k) {
-    if ($value) die2([$k, $value]);
-    $this->deleteTagItems($k);
-  }
-  */
-
-  function afterCreateUpdate($value, $k) {
+    return;
     $ids = [];
     foreach (explode(',', $value) as $v) if (($v = (int)$v)) $ids[] = $v;
     parent::afterCreateUpdate($ids, $k);
