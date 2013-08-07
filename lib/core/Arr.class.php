649bb484a1aff14902f9bebbde1edbe2988e8c49
masted <masted311@gmail.com>
2013-08-07 13:35:46 +0400

    ...

--- lib/core/Arr.class.php
+++ lib/core/Arr.class.php
@@ -95,8 +95,8 @@ class Arr {
     return isset($arr[$key]) ? $arr[$key] : false;
   }
 
-  static function &getValueByKey(array &$arr, $k1, $v1) {
-    foreach ($arr as &$v) if ($v[$k1] == $v1) return $v;
+  static function getValueByKey(array $arr, $k1, $v1) {
+    foreach ($arr as $v) if ($v[$k1] == $v1) return $v;
     return false;
   }
 
