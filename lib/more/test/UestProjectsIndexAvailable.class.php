2bb6a056a5887a4ef977f88a74f2054274e1efb6
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2013-09-24 10:34:23 +0400

    ...

--- lib/more/test/UestProjectsIndexAvailable.class.php
+++ lib/more/test/UestProjectsIndexAvailable.class.php
@@ -5,9 +5,7 @@ class TestProjectsIndexAvailable extends NgnTestCase {
   function test() {
     $projects = require NGN_ENV_PATH.'/config/projects.php';
     $curl = new Curl;
-    foreach ($projects as $v) {
-      $this->assertFalse((bool)strstr($curl->get("http://{$v['domain']}"), 'error:'), "index available {$v['domain']}");
-    }
+    foreach ($projects as $v) $this->assertFalse((bool)strstr($curl->get("http://{$v['domain']}"), 'error:'), "{$v['domain']}: index page not available");
   }
 
 }
