9b2e37fa73be3dcee55ac7fa6d4262a5435beb39
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2013-10-01 10:40:00 +0400

    ...

--- lib/more/queue/QueueWorker.class.php
+++ lib/more/queue/QueueWorker.class.php
@@ -14,17 +14,18 @@ class QueueWorker extends QueueBase {
     set_time_limit(0);
     output("Worker $this->id started");
 //  //LogWriter::str('worker', "worker $this->id init");
-    $this->getQueue()->consume(function(AMQPEnvelope $envelope) {
+    $this->getQueue()->consume(function (AMQPEnvelope $envelope) {
       $this->processData($envelope->getBody());
     }, AMQP_AUTOACK);
   }
 
   function processData($body) {
     output("Worker $this->id start processing data");
+    $t = getMicrotime();
     db()->disconnect();
     $this->_processData($body);
     db()->disconnect();
-    output("Worker $this->id finish processing data");
+    output("Worker $this->id finish processing data. Time: ".Misc::price(getMicrotime() - $t));
   }
 
   protected function _processData($body) {
@@ -61,19 +62,21 @@ class QueueWorker extends QueueBase {
       $o = unserialize($data['object']);
       if (isset($data['ljId']) and !is_subclass_of($o, 'LongJobAbstract')) throw new Exception('Object with class "'.get_class($o).'" must be subclass of "LongJobCycle"');
       if (isset($data['ljId'])) {
-    //  //LogWriter::str('worker', "$this->id started processing {$data['ljId']}");
+        //  //LogWriter::str('worker', "$this->id started processing {$data['ljId']}");
         output("status: {$data['ljId']}: ".LongJobCore::state($data['ljId'])->status());
       }
       $r = $o->{$data['method']}();
-  //  //if (isset($data['ljId'])) LogWriter::str('worker', "$this->id finished processing {$data['ljId']}. By ".($r ? 'complete' : 'abort'));
-    } else {
+      //  //if (isset($data['ljId'])) LogWriter::str('worker', "$this->id finished processing {$data['ljId']}. By ".($r ? 'complete' : 'abort'));
+    }
+    else {
       $class = ucfirst($data['class']);
       if ($data['method'] == '__construct') {
         new $class($data['data']);
-      } else {
+      }
+      else {
         (new $class)->{$data['method']}($data['data']);
       }
-  }
+    }
 
     unlink(DATA_PATH.'/queue/'.$id);
   }
