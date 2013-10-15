8a4a1de9e1fdd929ccfacce50aeb33b1c2fe5a1f
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2013-10-15 17:51:31 +0400

    ddXls fixes

--- lib/more/dd/DdXlsFake.class.php
+++ lib/more/dd/DdXlsFake.class.php
@@ -4,8 +4,6 @@ class DdXlsFake extends DdXls {
 
   function iteration() {
     parent::iteration();
-    LogWriter::str('ddXlsFake', $this->n.','.$this->step());
-    //LogWriter::str('ddXlsFake', $this->items->cond->all());
     file_put_contents(UPLOAD_PATH.$this->fileName.'.ids', ' ['.implode(', ', $this->items->getItemIds()).']', FILE_APPEND);
   }
 
