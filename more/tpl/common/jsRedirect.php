e9627d9bebdd8f631464bb9897c2ba10994438fe
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2013-11-18 15:30:44 +0400

    ...

--- more/tpl/common/jsRedirect.php
+++ more/tpl/common/jsRedirect.php
@@ -1,19 +1,5 @@
-<style>
-  .jsRedirect {
-    margin-left: 30%;
-    width: 400px;
-    padding: 50px 10px;
-  }
-</style>
-<div class="jsRedirect">
-  <? $sec = 3; ?>
-  Перенаправление на <a href="<?= $d['redirect'] ?>"><?= $d['redirect'] ?></a>
-  через <span id="countdown"><?= date('m/d/Y H:i:s', time() + $sec) ?></span>.
-</div>
-<script type="text/javascript">
-  new MooCountdown.Simple($('countdown'), {
-    onComplete: function() {
-      window.location = '<?= $d['redirect'] ?>';
-    }
-  });
+<script>
+  (function() {
+    window.location = '<?= $d['redirect'] ?>';
+  }).delay(100);
 </script>
