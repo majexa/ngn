6716a8d602bb7fa67e8818dede8886eea137238b
masted <masted311@gmail.com>
2013-11-22 13:02:51 +0400

    ...

--- lib/more/common/CtrlCommonClearTpl.class.php
+++ lib/more/common/CtrlCommonClearTpl.class.php
@@ -2,22 +2,9 @@
 
 class CtrlCommonClearTpl extends CtrlCommon {
 
-  protected function init() {
-    $this->hasOutput = false;
-  }
-  
   function action_default() {
-    $this->tt->tpl('clearTpl', ['tpl' => 'clearTpl/'.$this->req->path(2)]);
+    $this->d['mainTpl'] = 'clearTpl';
+    $this->d['tpl'] = 'clearTpl/'.$this->req->path(2);
   }
-  
-  function action_json_asd() {
-    return new Form(new Fields([
-      [
-        'title' => 'dqqdw',
-        'type' => 'wisiwigSimple',
-        'name' => 'asd',
-      ]
-    ]));
-  }
-  
+
 }
