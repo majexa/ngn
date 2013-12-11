24df50811e76527bbceefa44bca06fae5dd1cc59
snat <n.sosunova@e-promo.ru>
2013-12-11 11:07:08 +0400

    patch

---
+++ more/patches/db/1590_EmptyTagsFields.php
@@ -0,0 +1,7 @@
+<?php
+foreach (db()->selectCol('SELECT name FROM dd_structures') as $strName) {
+  $m = new DdFields($strName);
+  foreach (db()->cols("dd_i_$strName") as $name){
+    if(DdTags::isTagType($m->getType($name)) !== false) db()->query("UPDATE dd_i_$strName SET $name = '' ");
+  }
+}
