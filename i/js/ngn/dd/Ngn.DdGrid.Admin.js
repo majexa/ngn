d25390a92ac9776ed23c3c7ee4affe0763221625
masted <masted311@gmail.com>
2013-12-11 16:35:13 +0400

    ...

--- i/js/ngn/dd/Ngn.DdGrid.Admin.js
+++ i/js/ngn/dd/Ngn.DdGrid.Admin.js
@@ -29,6 +29,9 @@ Ngn.DdGrid.Admin = new Class({
  * @returns Ngn.DdGrid.Admin
  */
 Ngn.DdGrid.Admin.factory = function(strName, options) {
+  options = options || {};
+  options.resizeble = true;
+  options.id = 'dd' + strName;
   var clsP = eval('Ngn.DdGrid.Admin.' + ucfirst(strName));
   if (Ngn.projectKey && clsP) {
     var cls = eval('Ngn.DdGrid.Admin.' + ucfirst(strName) + '.' + ucfirst(Ngn.projectKey));
