0797ae3463aa1f8f818dd088f73b7c8db8e4d6f9
masted <masted311@gmail.com>
2014-01-14 17:03:44 +0400

    Commit was made from server localhost by ci/push

--- lib/more/dd/tags/DmfaDdTags.class.php
+++ lib/more/dd/tags/DmfaDdTags.class.php
@@ -6,13 +6,9 @@ class DmfaDdTags extends DmfaDdTagsAbstract {
     return $v ? implode(',', Arr::get($v, 'title')) : '';
   }
 
-  function form2sourceFormat($v) {
-    return Misc::quoted2arr(trim($v));
+  function afterCreateUpdate($v, $k) {
+    if (is_array($v)) $v = '';
+    DdTags::items($this->dm->strName, $k)->create($this->dm->id, Misc::quoted2arr($v));
   }
-
-  //function afterCreateUpdate($v, $k) {
-  //  if (is_array($v)) $v = '';
-  //  DdTags::items($this->dm->strName, $k)->create($this->dm->id, Misc::quoted2arr($v));
-  //}
 
 }
