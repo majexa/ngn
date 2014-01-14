b5f6fcb0705fd5ae7331bb41e58ac1f001fea033
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2014-01-14 13:23:29 +0400

    DmfaDdTagsTreeMultiselectAc: own source2formFormat

--- lib/more/dd/tags/DmfaDdTagsTreeMultiselectAc.class.php
+++ lib/more/dd/tags/DmfaDdTagsTreeMultiselectAc.class.php
@@ -2,6 +2,10 @@
 
 class DmfaDdTagsTreeMultiselectAc extends DmfaDdTagsTreeMultiselect {
 
+  function source2formFormat($v) {
+    return $v;
+  }
+
   function afterCreateUpdate($value, $k) {
     $ids = [];
     foreach (explode(',', $value) as $v) if (($v = (int)$v)) $ids[] = $v;
