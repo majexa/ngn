26e454c0fa49d984d4618da919c7c8f0a5953fcd
snat <n.sosunova@e-promo.ru>
2014-01-20 16:56:02 +0400

    ProjectData

--- lib/core/Arr.class.php
+++ lib/core/Arr.class.php
@@ -441,7 +441,7 @@ class Arr {
       return $v;
     }
     elseif (is_int($v)) return (int)$v;
-    else return $v; 
+    else return $v;
   }
 
   static function checkEmpty(array $arr, $keys, $quitely = false) {
