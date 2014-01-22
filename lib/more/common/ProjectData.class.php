aff1df7becd29bea49bcc646838f06e28918581b
masted <masted311@gmail.com>
2014-01-22 06:42:15 +0400

    Commit was made from server localhost by ci/push

--- lib/more/common/ProjectData.class.php
+++ lib/more/common/ProjectData.class.php
@@ -6,14 +6,4 @@ class ProjectData extends ProjectState {
     return DATA_PATH.'/data/'.$key.'.php';
   }
 
-  static function get($key, $quietly = false) {
-    $file = self::file($key);
-    $exists = file_exists($file);
-    if (!$quietly and !$exists) throw new NoFileException($file);
-    return $exists ? require $file : false;
-  }
-
-  static function update($key, $data) {
-    FileVar::updateVar(self::file($key), $data);
-  }
 }
