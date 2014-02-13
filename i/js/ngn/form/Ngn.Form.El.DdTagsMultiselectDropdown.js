fe249fe7f41a3ccea795e5fce2b2d45962d98c64
rawr <kkffs0@gmail.com>
2014-02-13 12:26:40 +0400

    DdTagsMultiselectDropdown

--- i/js/ngn/form/Ngn.Form.El.DdTagsMultiselectDropdown.js
+++ i/js/ngn/form/Ngn.Form.El.DdTagsMultiselectDropdown.js
@@ -4,45 +4,38 @@ Ngn.Form.El.DdTagsMultiselectDropdown = new Class({
   title: 'Выбрать',
 
   init: function() {
-    this.name = Ngn.frm.getPureName(this.eRow.getElement('input').get('name'));
     this.eFieldWrapper = this.eRow.getElement('.field-wrapper');
-    this.eChbxs = this.eFieldWrapper.getFirst();
-    this.eChbxs.addClass('multiselect-dropdown');
-    this.eOption = new Element('option');
-    this.toggle();
-    this.updateCount();
+    this.eChbxs = this.eFieldWrapper.getFirst().addClass('multiselect-dropdown');
+    this.eOption = new Element('option', {text: this.title});
+    this.toggleDropdown();
 
     new Element('select').grab(this.eOption).inject(this.eFieldWrapper, 'top');
-    this.eSelectOverride = new Element('div', {'class': 'select-override'}).inject(this.eFieldWrapper, 'top');
-    this.eOkBtn = (new Element(
+    this.eSelectOverride = new Element('div', {'class': 'select-override'}).addEvent('click', function(e) {
+      e.preventDefault();
+      this.toggleDropdown();
+    }.bind(this)).inject(this.eFieldWrapper, 'top');
+
+    (new Element(
       'div', {
         'class': 'iconsSet',
-        html: '<div class="image-button ok"><a id="time_ok" href="javascript:void(0)" class="btn" tabindex="0"><span><i></i>OK</span></a></div>',
-        events: {
-          click: function(e){
-            e.preventDefault();
-            this.toggle();
-          }
-        }
+        html: '<div class="image-button ok"><a id="time_ok" href="javascript:void(0)" class="btn" tabindex="0"><span><i></i>OK</span></a></div>'
       }
-    )).inject(this.eChbxs, 'bottom');
+    )).cloneEvents(this.eSelectOverride).inject(this.eChbxs, 'bottom');
 
     this.eChbxs.getElements('input').addEvent('click', function(e) {
-      this.updateCount();
+      this.updateTitle();
     }.bind(this));
-
-    this.eSelectOverride.addEvent('click', function(e) {
-      e.preventDefault();
-      this.toggle();
-    }.bind(this));
-    this.eOkBtn.cloneEvents(this.eSelectOverride);
   },
 
-  toggle: function() {
+  toggleDropdown: function() {
     this.eChbxs.style.left = (this.eChbxs.style.left == '-1111111111px') ? '' : '-1111111111px'
   },
 
-  updateCount: function() {
-    this.eOption.set('text',this.title+' ('+this.eChbxs.getElements('input:checked').length+')');
+  updateTitle: function() {
+    var titles = [];
+    this.eChbxs.getElements('input:checked').each(function(item) {
+      titles.push(item.getNext().get('text'));
+    });
+    this.eOption.set('text', titles.length > 0 ? titles.join(', '): this.title);
   }
 });
