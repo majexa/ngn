feadb2534fe07ad295122e45edecbe5b6c664944
snat <n.sosunova@e-promo.ru>
2014-03-04 18:34:01 +0400

    fieldOptions

--- lib/more/dd/tags/FieldEDdTagsSelect.class.php
+++ lib/more/dd/tags/FieldEDdTagsSelect.class.php
@@ -8,6 +8,7 @@ use DdElement;
   protected function init() {
     $opts = Arr::get(DdTags::get($this->strName, $this->options['name'])->getTags(), 'title', 'id');
     $this->options['options'] = $this->options['required'] ? $opts : ['' => '—'] + $opts;
+    if(!empty($this->options['ddFilter'])) $this->options['options'] = ['' => '—'] + Arr::filterByKeys($this->options['options'], $this->options['ddFilter']);
     parent::init();
   }
 
