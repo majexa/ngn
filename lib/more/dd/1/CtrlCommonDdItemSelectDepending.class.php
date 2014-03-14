3b5c31230054e90f68c2403c5d707d4e7d040826
snat <n.sosunova@e-promo.ru>
2014-03-14 15:13:22 +0400

    ...l

--- lib/more/dd/1/CtrlCommonDdItemSelectDepending.class.php
+++ lib/more/dd/1/CtrlCommonDdItemSelectDepending.class.php
@@ -4,7 +4,7 @@ class CtrlCommonDdItemSelectDepending extends CtrlCommon {
 
   function action_ajax_default() {
     $tags = new DdItems($this->req->reqNotEmpty('strName'));
-    if (!empty($this->req->r['itemSort'])) $tags->cond->setOrder($this->req->r['itemSort'].' ASC');
+    if (!empty($this->req->r['itemsSort'])) $tags->cond->setOrder($this->req->r['itemsSort']);
     $tags->addTagFilter($this->req->reqNotEmpty('parentTagFieldName'), $this->req->r['id']);
     $opt = $tags->getItems();
     if (empty($opt)) return;
