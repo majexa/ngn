17789c679abcddacd79da1e866aada84b93bf0f0
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2014-04-20 23:42:20 +0400

    ...

---
+++ more/lib/TestAaa.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class TestAaa extends ProjectTestCase {
+
+  function test() {
+    Sflm::setFrontendName('default');
+    Sflm::frontend('js')->addObject('Ngn.Form.El.DdTags', "ddTags field init");
+  }
+
+}
--- more/lib/test/TestCliCommon.class.php
+++ more/lib/test/TestCliCommon.class.php
@@ -10,4 +10,14 @@ class TestCliCommon {
     Cli::shell('php run.php "(new AllErrors)->clear()"');
   }
 
+  /**
+   * Отображает все, существующие в среде тесты
+   */
+  function lst() {
+    print O::get('CliColors')->getColoredString('proj g:', 'yellow')."\n";
+    foreach ((new TestRunnerProject('dummy'))->_g() as $class) print ClassCore::classToName('Test', $class)."\n";
+    print O::get('CliColors')->getColoredString('ngn run:', 'yellow')."\n";
+    foreach ((new TestRunnerNgn)->_getClasses() as $class) print ClassCore::classToName('Test', $class)."\n";
+  }
+
 }
