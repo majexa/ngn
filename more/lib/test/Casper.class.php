bdfb5f419a9f75e18471a46e6df40f71708c3c6d
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2014-08-16 16:52:06 +0400

    Commit was made from server localhost by ci/push

--- more/lib/test/Casper.class.php
+++ more/lib/test/Casper.class.php
@@ -2,14 +2,15 @@
 
 class Casper {
 
-  static function run($projectName, $data) {
+  static function run($projectName, array $steps, array $data = []) {
     $projectDir = NGN_ENV_PATH.'/projects/'.$projectName;
+    $data['steps'] = $steps;
     $data = json_encode($data);
     if (strstr($data, "'")) throw new Exception('Data can not contains single quotes');
     $casperFolder = NGN_PATH.'/more/casper';
     $cmd = "echo '$data' | casperjs $casperFolder/test.js --projectDir=$projectDir";
-    //output("running $cmd");
-    $process = proc_open($cmd, [['pipe', 'r'],['pipe', 'w'],['pipe', 'w']], $pipes, realpath('./'), []);
+    //output($cmd);
+    $process = proc_open($cmd, [['pipe', 'r'], ['pipe', 'w'], ['pipe', 'w']], $pipes, realpath('./'), []);
     $buffer = '';
     if (is_resource($process)) {
       while ($s = fgets($pipes[1])) {
