310b496f290783559536fd767e1a362a5be9f8b6
masted <masted311@gmail.com>
2014-09-17 17:03:43 +0400

    Auto-commit from localhost

--- more/scripts/js/common/Ngn.php
+++ more/scripts/js/common/Ngn.php
@@ -1,7 +1,7 @@
 // -- Dynamic Core --
 
 Ngn.projectKey = '<?= PROJECT_KEY ?>';
-Ngn.isDebug = true;<?= IS_DEBUG ? 'true' : 'false' ?>;
+Ngn.isDebug = <?= getConstant('IS_DEBUG') ? 'true' : 'false' ?>;
 Ngn.fileSizeMax = <?= Misc::phpIniFileSizeToBytes(ini_get('upload_max_filesize')) ?>;
 Ngn.sessionId = '<?= session_id() ?>';
 Ngn.siteTitle = '<?= SITE_TITLE ?>';
