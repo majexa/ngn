34fa0eb696a25c8e627908028b784f76dc824c97
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2014-11-26 08:30:28 +0400

    Auto-commit on 26.11.2014 08:30:27

--- more/lib/core/Error404.class.php
+++ more/lib/core/Error404.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class Error404 extends Exception {
+class Error404 extends NotLoggableError {
 
   public function __construct($message = '', $code = 0, Exception $previous = null) {
     if (!$message) $message = '404 error';
