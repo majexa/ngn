b8565780dc37f46454993694f84bf8ed17b54df9
masted <masted311@gmail.com>
2015-01-02 17:35:34 +0300

    Auto-commit on 02.01.2015 18:35:33

--- init/site.php
+++ init/site.php
@@ -3,6 +3,7 @@
 require_once NGN_PATH.'/init/core.php';
 if (file_exists(PROJECT_PATH.'/config/constants/site.php')) require PROJECT_PATH.'/config/constants/site.php';
 if (file_exists(PROJECT_PATH.'/config/constants/database.php')) require PROJECT_PATH.'/config/constants/database.php';
-if (!defined('PROJECT_PATH') or !is_dir(PROJECT_PATH)) die2('Dir PROJECT_PATH not defined or "'.PROJECT_PATH.'"');
+if (!defined('PROJECT_PATH')) die2('PROJECT_PATH not defined');
+if (!is_dir(PROJECT_PATH)) die2('PROJECT_PATH "'.die2('PROJECT_PATH not defined').'" is not directory');
 setConstant('SITE_LIB_PATH', PROJECT_PATH.'/lib');
 Ngn::addBasePath(PROJECT_PATH, 5);
