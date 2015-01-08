a89caa7f18e9194cc5a6b7c421c3cdb2e3b1e590
masted <masted311@gmail.com>
2015-01-08 12:49:46 +0300

    Auto-commit on 08.01.2015 13:49:45

--- more/lib/users/TestUiAuthDialog.class.php
+++ more/lib/users/TestUiAuthDialog.class.php
@@ -3,6 +3,8 @@
 class TestUiAuthDialog extends ProjectTestCase {
 
   function test() {
+    SiteConfig::replaceConstant('more', 'TESTING', true);
+    SiteConfig::updateSubVar('userReg', 'emailEnable', false);
     SiteConfig::updateSubVar('userReg', 'enable', true);
     SiteConfig::updateSubVar('userReg', 'phoneEnable', true);
     SiteConfig::updateSubVar('userReg', 'phoneConfirm', true);
