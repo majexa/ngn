b3de468aa3ce2a2cf4655b86bd5404fbac771781
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2015-03-13 09:22:32 +0300

    Auto-commit on 13.03.2015 10:22:31

--- i/js/ngn/Ngn.CalendarMonthSlider.js
+++ i/js/ngn/Ngn.CalendarMonthSlider.js
@@ -2,48 +2,24 @@
 Ngn.CalendarMonthSlider = new Class({
 
   initialize: function(eHeader) {
+    this.currentMonth = eHeader.get('data-m');
+    this.currentYear = eHeader.get('data-y');
     this.btnPrev = eHeader.getElement('.prev');
     this.btnNext = eHeader.getElement('.next');
     this.el = eHeader.getElement('.current');
-    var nextMonthYear = function(month, year) {
-      if (month == 12) return [1, parseInt(year)+1];
-      else return [parseInt(month)+1, parseInt(year)];
-    };
-    var prevMonthYear = function(month, year) {
-      if (month == 1) return [12, parseInt(year)-1];
-      else return [parseInt(month)-1, parseInt(year)];
-    };
+    var o = this;
     var fxNext = new Fx.Slide(this.el, {
       mode: 'horizontal',
       onComplete: function() {
         if (this.from[0] != 0) return;
-        var nextTitle = this.element.get('href').replace(new RegExp('.*d\\.(\\d+);(\\d+).*'), function(s, month, year) {
-          var next = nextMonthYear(month, year);
-          return Ngn.config.ruMonths[next[0]] + ' ' + next[1];
-        });
-        var nextLink = this.element.get('href').replace(new RegExp('d\\.(\\d+);(\\d+)'), function(s, month, year) {
-          var next = nextMonthYear(month, year);
-          return 'd.' + next[0] + ';' + next[1];
-        });
-        console.debug(nextTitle);
-        this.element.set('html', nextTitle);
-        this.element.set('href', nextLink);
+        o.changeLinkElement(o.nextMonthYear(), this.element);
       }
     });
     var fxPrev = new Fx.Slide(this.el, {
       mode: 'horizontal',
       onComplete: function() {
         if (this.from[0] != 0) return;
-        var prevTitle = this.element.get('href').replace(new RegExp('.*d\\.(\\d+);(\\d+).*'), function(s, month, year) {
-          var prev = prevMonthYear(month, year);
-          return Ngn.config.ruMonths[prev[0]] + ' ' + prev[1];
-        });
-        var prevLink = this.element.get('href').replace(new RegExp('d\\.(\\d+);(\\d+)'), function(s, month, year) {
-          var prev = prevMonthYear(month, year);
-          return 'd.' + prev[0] + ';' + prev[1];
-        });
-        this.element.set('html', prevTitle);
-        this.element.set('href', prevLink);
+        o.changeLinkElement(o.prevMonthYear(), this.element);
       }
     });
     this.btnNext.addEvent('click', function(e) {
@@ -54,6 +30,23 @@ Ngn.CalendarMonthSlider = new Class({
       e.preventDefault();
       fxPrev.slideOut().chain(fxPrev.slideIn);
     });
+  },
+
+  nextMonthYear: function() {
+    if (this.currentMonth == 12) return [1, parseInt(this.currentYear)+1];
+    else return [parseInt(this.currentMonth)+1, parseInt(this.currentYear)];
+  },
+
+  prevMonthYear: function() {
+    if (this.currentMonth == 1) return [12, parseInt(this.currentYear)-1];
+    else return [parseInt(this.currentMonth)-1, parseInt(this.currentYear)];
+  },
+
+  changeLinkElement: function(data, el) {
+    this.currentMonth = data[0];
+    this.currentYear = data[1];
+    el.set('html', Ngn.config.ruMonths[data[0]] + ' ' + data[1]);
+    el.set('href', 'd.' + data[0] + ';' + data[1]);
   }
 
 });
