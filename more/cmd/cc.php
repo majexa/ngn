a0c72d3863f276f3effb9df6e061ffb283d11cd3
masted <masted311@gmail.com>
2015-05-04 14:52:05 +0300

    Auto-commit on 04.05.2015 15:52:04

--- more/cmd/cc.php
+++ more/cmd/cc.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once CORE_PATH.'/lib/Memc.class.php';
-require_once CORE_PATH.'/lib/Mem.class.php';
+//require_once CORE_PATH.'/lib/Memc.class.php';
+//require_once CORE_PATH.'/lib/Mem.class.php';
 require_once MORE_PATH.'/lib/core/UrlCache.class.php';
 require_once MORE_PATH.'/lib/sflm/SflmBase.class.php';
 require_once MORE_PATH.'/lib/sflm/SflmJs.class.php';
@@ -9,7 +9,7 @@ require_once MORE_PATH.'/lib/sflm/SflmCss.class.php';
 require_once MORE_PATH.'/lib/sflm/Sflm.class.php';
 require_once MORE_PATH.'/lib/dd/DdiCache.class.php';
 FileCache::c()->clean();
-Mem::clean();
+//Mem::clean();
 UrlCache::clearCache();
 DdiCache::cleanAll();
 Sflm::clearCache();
