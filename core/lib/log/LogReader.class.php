ff5d361c930d7b5eb9353517940224b9f261fecb
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2015-05-16 11:28:41 +0300

    Auto-commit on 16.05.2015 12:28:41

--- core/lib/log/LogReader.class.php
+++ core/lib/log/LogReader.class.php
@@ -26,36 +26,20 @@ class LogReader {
   static function _get($file) {
     $a = function ($file) {
       $handle = fopen($file, 'r');
-      if ($handle) {
-        $c = '';
-        while (($buffer = fgets($handle, 4096)) !== false) {
-          $c .= $buffer;
-          if (strstr($c, '=====+=====')) {
-            $p = explode('=====+=====', $c)[0];
-            $c = '';
-            yield $p;
-          }
+      $c = '';
+      while (($buffer = fgets($handle, 4096)) !== false) {
+        $c .= $buffer;
+        if (strstr($c, "\n")) {
+          $p = explode("\n", $c)[0];
+          $c = '';
+          yield $p;
         }
-        fclose($handle);
       }
+      fclose($handle);
     };
     $b = function () use ($file, $a) {
       foreach ($a($file) as $p) {
-        if (!preg_match('/(\d+.\d+.\d+ \d+.\d+.\d+): \((.*)\)\n(.*)\n<body>(.*)<\/body>\n<trace>(.*)<\/trace>/ms', $p, $m)) continue;
-        $v = [];
-        $v['time'] = strtotime($m[1]);
-        $v['body'] = str_replace("\\'", '', $m[4]);
-        $v['file'] = $file;
-        $v['trace'] = $m[5];
-        if ($m[3]) {
-          if (!preg_match_all('/^([a-z]+): {(.*)},?/iU', $m[3], $mm)) {
-            throw new Exception("wrong param string '{$m[3]}'");
-          }
-          foreach ($mm[1] as $k => $paramName) {
-            $v[$paramName] = $mm[2][$k];
-          }
-        }
-        if (preg_match('/<post>(.*)<\/post>/ms', $p, $m)) $v['post'] = $m[1];
+        $v = json_decode($p, JSON_FORCE_OBJECT);
         yield $v;
       }
     };
