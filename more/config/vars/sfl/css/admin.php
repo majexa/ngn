2268f5125646bc4feb09f58b9efd8e77a163efe6
masted <masted311@gmail.com>
2015-07-16 15:24:18 +0300

    Auto-commit on 16.07.2015 16:24:17

--- more/config/vars/sfl/css/admin.php
+++ more/config/vars/sfl/css/admin.php
@@ -5,7 +5,6 @@ return [
   'i/css/common/autocompleter.css',
   'i/css/admin/panel.css',
   'i/css/admin/mainContent.css',
-  'i/css/admin/ddo.css',
   'i/css/admin/ddFilters.css',
   'i/css/admin/grid.css',
   'i/css/admin/pNums.css',
