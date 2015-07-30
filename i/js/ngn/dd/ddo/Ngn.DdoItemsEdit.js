78454576b764dca0f4a741c4d0b0d9a8b1cc5a48
masted <masted311@gmail.com>
2015-07-30 16:55:20 +0300

    Auto-commit on 30.07.2015 17:55:19

--- i/js/ngn/dd/ddo/Ngn.DdoItemsEdit.js
+++ i/js/ngn/dd/ddo/Ngn.DdoItemsEdit.js
@@ -14,6 +14,17 @@ Ngn.DdoItemsEdit = new Class({
         baseUrl: this.options.baseUrl,
         onEditComplete: function(id) {
           this.reloadItem(id);
+        }.bind(this),
+        onDeleteComplete: function(id) {
+          new Fx.Morph(eItem, {
+            onComplete: function() {
+              eItem.destroy();
+            }
+          }).start({
+              height: 0,
+              'margin-right': 0,
+              opacity: 0
+            });
         }.bind(this)
       }
     );
