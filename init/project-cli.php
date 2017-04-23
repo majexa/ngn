6122e37c9c77b2b1e3eb7befe1e2d7972698b030
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-04-23 21:11:17 +0300

    add include database config to project-cli init

--- init/project-cli.php
+++ init/project-cli.php
@@ -7,3 +7,15 @@ require_once PROJECT_PATH.'/config/constants/core.php';
 require_once NGN_PATH.'/init/site.php';
 require_once NGN_PATH.'/init/more.php';
 require_once NGN_PATH.'/init/cli.php';
+
+Lib::requireIfExists(PROJECT_PATH.'/config/constants/database.php');
+
+if (file_exists(NGN_ENV_PATH.'/config/database.php')) {
+  require NGN_ENV_PATH.'/config/database.php';
+  if (!defined('DB_NAME')) {
+    throw new Exception('DB_NAME still ont defined after including '.NGN_ENV_PATH.'/config/database.php');
+  }
+  if (!defined('DB_USER')) {
+    throw new Exception('DB_USER still ont defined after including '.NGN_ENV_PATH.'/config/database.php');
+  }
+}
